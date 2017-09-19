9e13e5d0512108b5ab5ac081e87b156e6c4f18d4
Wilson <wilson.ow@xy-connect.com>
2017-09-19 17:03:55 +0800

    Added comments

--- Skiing.java
+++ Skiing.java
@@ -5,110 +5,110 @@ import java.util.Scanner;
 
 public class Skiing {
 
-    private static final String FILENAME = "Example_Input_2.txt";
+  private static final String FILENAME = "Example_Input_2.txt";
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      // Obtain 2d array digital map
-      int[][] map = getMap2dArray(FILENAME);
+    // Obtain 2d array digital map
+    int[][] map = getMap2dArray(FILENAME);
 
-      // If input was valid
-      if (map != null) {
-          String path = "";
-          String longestPath = "";
-          String currentPath = "";
+    // If input was valid
+    if (map != null) {
+      String path = "";
+      String longestPath = "";
+      String currentPath = "";
 
-					// Loop through every single element and seek the longest path it can obtain
-          for (int i = 0; i < map.length; i++) {
-              for (int j = 0; j < map[0].length; j++) {
+			// Loop through every single element and seek the longest path it can obtain
+      for (int i = 0; i < map.length; i++) {
+        for (int j = 0; j < map[0].length; j++) {
 
-                  currentPath = recursiveSeek(map, i, j, path);
+          currentPath = recursiveSeek(map, i, j, path);
 
-                  // If returned path from this element is longer than
-                  // the current stored path, save it as the lonest path.
-                  if (currentPath.length() > longestPath.length())
-                      longestPath = currentPath;
+          // If returned path from this element is longer than
+          // the current stored path, save it as the lonest path.
+          if (currentPath.length() > longestPath.length())
+              longestPath = currentPath;
 
-                  // If the returned path from this element is the same length as
-                  // the current stored path, save the one with the more vertical drop.
-                  else if (currentPath.length() == longestPath.length()) {
-                      int currentPathDrop = Character.getNumericValue(currentPath.charAt(0)) - Character.getNumericValue(currentPath.charAt(currentPath.length()-1));
-                      int longestPathDrop = Character.getNumericValue(longestPath.charAt(0)) - Character.getNumericValue(longestPath.charAt(longestPath.length()-1));
-                      if (currentPathDrop > longestPathDrop) {
-                          longestPath = currentPath;
-                      }
-                  }
-              }
+          // If the returned path from this element is the same length as
+          // the current stored path, save the one with the more vertical drop.
+          else if (currentPath.length() == longestPath.length()) {
+            int currentPathDrop = Character.getNumericValue(currentPath.charAt(0)) - Character.getNumericValue(currentPath.charAt(currentPath.length()-1));
+            int longestPathDrop = Character.getNumericValue(longestPath.charAt(0)) - Character.getNumericValue(longestPath.charAt(longestPath.length()-1));
+            if (currentPathDrop > longestPathDrop) {
+                longestPath = currentPath;
+            }
           }
-          System.out.println("Longest and largest vertical drop path: " + longestPath);
-      } else {
-          System.out.println("Input out of range!");
+        }
       }
+        System.out.println("Longest and largest vertical drop path: " + longestPath);
+    } else {
+        System.out.println("Input out of range!");
     }
+  }
 
   private static int[][] getMap2dArray(String fileName) {
 
-      BufferedReader br = null;
-      FileReader fr = null;
-      int numLine = 0; // current number of line
-      try {
-          fr = new FileReader(FILENAME);
-          br = new BufferedReader(fr);
+    BufferedReader br = null;
+    FileReader fr = null;
+    int numLine = 0; // current number of line
+    try {
+        fr = new FileReader(FILENAME);
+        br = new BufferedReader(fr);
 
-          String sCurrentLine;
-          int[][] elevationArray = new int[10][10];
+        String sCurrentLine;
+        int[][] elevationArray = new int[10][10];
 
-                      while ((sCurrentLine = br.readLine()) != null) {
-              // Get map size
-              if (numLine == 0) {
-                  String[] size = sCurrentLine.split(" ");
-                  System.out.println("Map size: " + size[0] + "x" + size[1]);
-                  int numRow = Integer.parseInt(size[0]);
-                  int numCol = Integer.parseInt(size[1]);
+        while ((sCurrentLine = br.readLine()) != null) {
+          // Get map size
+          if (numLine == 0) {
+              String[] size = sCurrentLine.split(" ");
+              System.out.println("Map size: " + size[0] + "x" + size[1]);
+              int numRow = Integer.parseInt(size[0]);
+              int numCol = Integer.parseInt(size[1]);
 
-                  // Make sure the program is not fed with something out of the range
-                  if (numRow > 0 && numRow <= 1000 && numCol> 0 && numCol <= 1000)
-                      elevationArray = new int[numRow][numCol];
-                  else {
-                      return null;
-                  }
-              } else {
-                  String[] splitLine = sCurrentLine.split(" ");
-
-                  // If there is one row that is not having the same width stated, it is not a valid input.
-                  if (splitLine.length != elevationArray[0].length) {
-                      System.out.println("Invalid number of columns");
-                      return null;
-                  }
-                  // Save elevation into 2-d array
-                  for (int i = 0; i < elevationArray[0].length; i++) {
-                      elevationArray[numLine-1][i] = Integer.parseInt(splitLine[i]);
-                  }
+              // Make sure the program is not fed with something out of the range
+              if (numRow > 0 && numRow <= 1000 && numCol> 0 && numCol <= 1000)
+                  elevationArray = new int[numRow][numCol];
+              else {
+                  return null;
               }
-              numLine++;
-          }
-          // If the number of rows is not the same height stated, it is not a valid input. (no. of rows = total lines - 1)
-          if (numLine-1 != elevationArray.length) {
-              System.out.println("Invalid number of rows");
-              return null;
-          }
-          return elevationArray;
-      } catch (IOException e) {
-          e.printStackTrace();
-      } finally {
-              try {
+          } else {
+              String[] splitLine = sCurrentLine.split(" ");
 
-                  if (br != null)
-                      br.close();
-
-                  if (fr != null)
-                      fr.close();
-
-              } catch (IOException ex) {
-                  ex.printStackTrace();
+              // If there is one row that is not having the same width stated, it is not a valid input.
+              if (splitLine.length != elevationArray[0].length) {
+                  System.out.println("Invalid number of columns");
+                  return null;
               }
-      }
-      return null;
+              // Save elevation into 2-d array
+              for (int i = 0; i < elevationArray[0].length; i++) {
+                  elevationArray[numLine-1][i] = Integer.parseInt(splitLine[i]);
+              }
+          }
+          numLine++;
+        }
+        // If the number of rows is not the same height stated, it is not a valid input. (no. of rows = total lines - 1)
+        if (numLine-1 != elevationArray.length) {
+          System.out.println("Invalid number of rows");
+          return null;
+        }
+        return elevationArray;
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+
+          if (br != null)
+              br.close();
+
+          if (fr != null)
+              fr.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    return null;
   }
 
   private static String recursiveSeek(int[][] map, int i, int j, String path) {
